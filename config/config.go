a7cbf7c1d24a69ed75b53bbc7bb9d2dd94068671
EssPi <dev.shahryar@gmail.com>
2024-11-04 14:30:36 +0330

    Modify env : Add telegram token

--- config/config.go
+++ config/config.go
@@ -22,6 +22,7 @@ type Config struct {
 	DatabaseMaxIdleConns    int           `mapstructure:"DATABASE_MAX_IDLE_CONNS"`
 	DatabaseMaxOpenConns    int           `mapstructure:"DATABASE_MAX_OPEN_CONNS"`
 	DatabaseConnMaxLifetime time.Duration `mapstructure:"DATABASE_CONN_MAX_LIFETIME"`
+	BotToken                string        `mapstructure:"BOT_TOKEN"`
 }
 
 func loadEnvConfig() (*viper.Viper, error) {
