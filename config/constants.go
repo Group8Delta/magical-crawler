4af97410bf46302a4de3728e47d1cf52935e1a01
EssPi <dev.shahryar@gmail.com>
2024-11-06 11:20:06 +0330

    add constants Bot values

---
+++ config/constants.go
@@ -0,0 +1,68 @@
+package config
+
+// Bot
+// Main Menu
+const (
+	MainMenu                = "منوی اصلی"
+	SearchButton            = "جستجو"
+	FiltersButton           = "فیلتر ها"
+	FavoritesButton         = "لیست علاقه‌مندی ها"
+	ExportButton            = "خروجی فایل"
+	AccountManagementButton = "مدیریت حساب"
+	SupportButton           = "تماس با ما"
+	BackButton              = "بازگشت"
+)
+
+// Search Menu
+const (
+	SourceSelectButton   = "انتخاب منبع"
+	KeywordSearchButton  = "کلید واژه ها"
+	SearchSettingsButton = "تنظیمات جستجو"
+	StartSearchButton    = "شروع جستجو"
+)
+
+// Filters Menu
+const (
+	PriceFilter         = "قیمت"
+	AreaFilter          = "متراژ"
+	RoomsFilter         = "تعداد اتاق خواب"
+	PropertyTypeFilter  = "نوع ملک"
+	BuildingAgeFilter   = "سن بنا"
+	FloorFilter         = "طبقه"
+	StorageFilter       = "انبار"
+	ElevatorFilter      = "آسانسور"
+	AdDateFilter        = "تاریخ ایجاد آگهی"
+	LocationFilter      = "موقعیت"
+	RemoveFiltersButton = "پاک کردن فیلتر ها"
+	ApplyFiltersButton  = "اعمال فیلتر"
+)
+
+// Account Management Menu
+const (
+	SetUsernameButton    = "ویرایش نام کاربری"
+	ShareBookmarksButton = "اشتراک گذاری علاقه‌مندی‌ها"
+	LogoutButton         = "خروج از حساب"
+)
+
+// Search Results Menu
+const (
+	NextResultButton      = "نتیجه بعدی"
+	PreviousResultButton  = "نتیجه قبلی"
+	SaveToFavoritesButton = "ذخیره در علاقه‌مندی‌ها"
+	ViewDetailsButton     = "نمایش جزئیات"
+	BackToSearchButton    = "بازگشت به جستجو"
+)
+
+// Export Menu
+const (
+	DownloadZipButton   = "دانلود فایل ZIP"
+	SendEmailButton     = "ارسال به ایمیل"
+	FileFormatSelection = "انتخاب فرمت فایل (CSV یا XLSX)"
+)
+
+// Crawler Admin Menu
+const (
+	CrawlerStatusButton    = "مشاهده وضعیت کرالر"
+	PeriodicSettingsButton = "تنظیمات دوره‌ای"
+	ViewLogsButton         = "مشاهده لاگ‌ها"
+)
