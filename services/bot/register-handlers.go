19227329f2692bc0a01939a6fcc1515fc0d2307c
EssPi <dev.shahryar@gmail.com>
2024-11-07 22:07:57 +0330

    modify main menu

--- services/bot/register-handlers.go
+++ services/bot/register-handlers.go
@@ -3,8 +3,7 @@ package bot
 import "magical-crwler/config"
 
 func RegisterHanlders(b *Bot) {
-	b.bot.Handle("/menu", MenuHandler(b))
 	b.bot.Handle("/start", StartHandler(b))
-	b.bot.Handle(config.FiltersButton, FilterHandlers(b))
+	b.bot.Handle(config.SearchButton, FilterHandlers(b))
 	b.bot.Handle("/exportFile", ExportHandler(b))
 }
--- services/bot/start-handler.go
+++ services/bot/start-handler.go
@@ -9,18 +9,24 @@ import (
 
 func StartHandler(b *Bot) func(ctx telebot.Context) error {
 	return func(ctx telebot.Context) error {
-		menu := &telebot.ReplyMarkup{
-			ResizeKeyboard: true,
-		}
-
-		filterBtn := menu.Text(config.FiltersButton)
-
-		menu.Reply(
-			menu.Row(filterBtn),
-		)
-
 		user := ctx.Sender()
 		log.Printf("%s %s | %d started bot", user.FirstName, user.LastName, user.ID)
+
+		var (
+			menu = &telebot.ReplyMarkup{ResizeKeyboard: true}
+
+			searchBtn   = menu.Text(config.SearchButton)
+			filtersBtn  = menu.Text(config.FiltersButton)
+			accountBtn  = menu.Text(config.AccountManagementButton)
+			exportBtn   = menu.Text(config.ExportButton)
+			bookmarkBtn = menu.Text(config.FavoritesButton)
+		)
+
+		menu.Reply(
+			menu.Row(searchBtn, filtersBtn),
+			menu.Row(exportBtn, bookmarkBtn),
+			menu.Row(accountBtn),
+		)
 
 		return ctx.Send(config.WelcomeMsg, menu)
 	}
