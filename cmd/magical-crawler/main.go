c87d00e5e1ab08d3aea43143b28b33334f3d07ec
amirjbr <amirreza.jabbari2023@gmail.com>
2024-11-11 01:44:14 +0330

    something magical happend in app :)

--- cmd/magical-crawler/main.go
+++ cmd/magical-crawler/main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"magical-crwler/config"
-	"magical-crwler/database"
-	"net/http"
+	"log"
+	"magical-crwler/services/bot"
+	"time"
 )
 
 func main() {
-	config := config.GetConfig()
+	// config := config.GetConfig()
 
-	database := database.New()
-	database.Init(config)
-	defer database.Close()
+	// database := database.New()
+	// database.Init(config)
+	// defer database.Close()
 
-	db, err := database.GetDb().DB()
-	if err != nil {
-		fmt.Println("database connection error", err)
-	}
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("database connection error", err)
-	}
-  // I commented on this part because it needs a VPN to run 
-	// bot, err := bot.NewBot(bot.BotConfig{
-	// 	Token:  config.BotToken,
-	// 	Poller: 10 * time.Second,
-	// })
+	// db, err := database.GetDb().DB()
 	// if err != nil {
-	// 	log.Println(err.Error())
+	// 	fmt.Println("database connection error", err)
 	// }
-	// bot.StartBot()
-	http.ListenAndServe(":"+config.Port, nil)
+	// err = db.Ping()
+	// if err != nil {
+	// 	fmt.Println("database connection error", err)
+	// }
+	// I commented on this part because it needs a VPN to run
+	bot, err := bot.NewBot(bot.BotConfig{
+		Token:  "7613959952:AAFGj8EbkaTqgih0Eh_xjoHiLS2iExyL7PU",
+		Poller: 10 * time.Second,
+	})
+	if err != nil {
+		log.Println(err.Error())
+	}
+	bot.StartBot()
+	// http.ListenAndServe(":"+config.Port, nil)
 }
