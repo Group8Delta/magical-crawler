6cd85e4d002b01da227301b04acb5f92f3496ac6
sajadblnyn <sajadbalaniyan@gmail.com>
2024-11-13 18:39:39 +0330

    feat: add repository functions for save crawler data

--- database/repository.go
+++ database/repository.go
@@ -3,6 +3,7 @@ package database
 import (
 	"magical-crwler/models"
 	"magical-crwler/models/Dtos"
+	"time"
 )
 
 type IRepository interface {
@@ -73,6 +74,78 @@ func (r *Repository) UpdateFilter(filter Dtos.FilterDto) (models.Filter, error) 
 
 func (r *Repository) DeleteFilter(id int) {
 	r.db.GetDb().Where("ID = ?", id).Delete(&models.Filter{})
+}
+
+func (r *Repository) GetAdByLink(link string) (models.Ad, error) {
+	ad := models.Ad{}
+	res := r.db.GetDb().Where("link = ?", link).First(&ad)
+	return ad, res.Error
+}
+
+func (r *Repository) CreateAd(ad Dtos.AdDto) models.Ad {
+	adm := models.Ad{
+		Link:          ad.Link,
+		PhotoUrl:      ad.PhotoUrl,
+		SellerName:    ad.SellerName,
+		SellerContact: ad.SellerContact,
+		Description:   ad.Description,
+		Price:         ad.Price,
+		RentPrice:     ad.RentPrice,
+		City:          ad.City,
+		Neighborhood:  ad.Neighborhood,
+		Size:          ad.Size,
+		Bedrooms:      ad.Bedrooms,
+		HasElevator:   ad.HasElevator,
+		HasStorage:    ad.HasStorage,
+		BuiltYear:     ad.BuiltYear,
+		ForRent:       ad.ForRent,
+		IsApartment:   ad.IsApartment,
+		Floor:         ad.Floor,
+		CreationTime:  ad.CreationTime,
+	}
+
+	r.db.GetDb().Create(&adm)
+	return adm
+}
+
+func (r *Repository) UpdateAd(ad Dtos.AdDto) (models.Ad, error) {
+	a := models.Ad{}
+	res := r.db.GetDb().Where("id = ?", ad.ID).First(&a)
+	if res.Error != nil {
+		return a, res.Error
+	}
+
+	a.Link = ad.Link
+	a.PhotoUrl = ad.PhotoUrl
+	a.SellerName = ad.SellerName
+	a.SellerContact = ad.SellerContact
+	a.Description = ad.Description
+	a.Price = ad.Price
+	a.RentPrice = ad.RentPrice
+	a.City = ad.City
+	a.Neighborhood = ad.Neighborhood
+	a.Size = ad.Size
+	a.Bedrooms = ad.Bedrooms
+	a.HasElevator = ad.HasElevator
+	a.HasStorage = ad.HasStorage
+	a.BuiltYear = ad.BuiltYear
+	a.ForRent = ad.ForRent
+	a.IsApartment = ad.IsApartment
+	a.Floor = ad.Floor
+	r.db.GetDb().Save(&a)
+	return a, nil
+}
+
+func (r *Repository) CreatePriceHistory(ph Dtos.PriceHistoryDto) models.PriceHistory {
+	p := models.PriceHistory{
+		AdID:        ph.AdID,
+		Price:       ph.Price,
+		RentPrice:   ph.RentPrice,
+		SubmittedAt: time.Now(),
+	}
+
+	r.db.GetDb().Create(&p)
+	return p
 }
 
 func NewRepository(db DbService) *Repository {
