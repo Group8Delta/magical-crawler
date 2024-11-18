68abe156cc9c2c0fcf3691680894692496c1ccbb
EssPi <dev.shahryar@gmail.com>
2024-11-19 02:02:42 +0330

    remove db from filters

--- services/bot/bot.go
+++ services/bot/bot.go
@@ -77,7 +77,7 @@ func (b *Bot) RegisterHandlers(db *gorm.DB) {
 	})
 	b.Bot.Handle("/start", StartHandler(b, db))
 	b.Bot.Handle(&telebot.Btn{Unique: "export"}, ExportHandler(b))
-	b.Bot.Handle(constants.SearchButton, SearchHandlers(b, db))
+	b.Bot.Handle(constants.SearchButton, SearchHandlers(b))
 	b.Bot.Handle("/exportFile", ExportHandler(b))
 	b.Bot.Handle(&telebot.Btn{Unique: "export"}, ExportHandler(b))
 	b.Bot.Handle(&telebot.Btn{Unique: "export_csv"}, export_csv_Handler(b))
