6d6456cc1f22fbf56a48fca4f026641be379e106
EssPi <dev.shahryar@gmail.com>
2024-11-18 23:30:09 +0330

    fix a bug in message generator

--- utils/message_generator.go
+++ utils/message_generator.go
@@ -11,13 +11,12 @@ func GenerateFilterMessage(ad models.Ad) string {
 	var builder strings.Builder
 	builder.WriteString("<b>آگهی‌های جدید:</b>\n\n")
 
-	builder.WriteString(fmt.Sprintf("<b%s</b> <a href='%s'>مشاهده آگهی</a>\n", ad.Link, ad.Link))
+	builder.WriteString(fmt.Sprintf("<b>%s</b> <a href='%s'>مشاهده آگهی</a>\n", ad.Link, ad.Link))
 	builder.WriteString(fmt.Sprintf("<b>فروشنده:</b> %s\n", ad.SellerName))
 
 	if ad.Description != nil {
 		builder.WriteString(fmt.Sprintf("<b>توضیحات:</b> %s\n", *ad.Description))
 	}
-
 	if ad.Price != nil {
 		builder.WriteString(fmt.Sprintf("<b>قیمت:</b> %d تومان\n", *ad.Price))
 	}
