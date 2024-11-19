f25282d24021e57828c82d1134a61da68265ec44
fatemeh <fatemeh@tural.com>
2024-11-20 02:39:58 +0330

    Refactor search handler

--- models/filtered_ad.go
+++ models/filtered_ad.go
@@ -4,9 +4,9 @@ import "time"
 
 type FilteredAd struct {
 	ID        uint
-	UserID    uint
-	FilterID  uint
-	AdID      uint
+	UserID    uint `gorm:"not null;uniqueIndex:idx_user_filter_ad"`
+	FilterID  uint `gorm:"not null;uniqueIndex:idx_user_filter_ad"`
+	AdID      uint `gorm:"not null;uniqueIndex:idx_user_filter_ad"`
 	TimeStamp time.Time
 	Filter    Filter
 	Ad        Ad
